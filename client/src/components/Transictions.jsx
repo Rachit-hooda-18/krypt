93913aca6a5bc682d7b7a64ed4a38330779c1961
Rachit-hooda-18 <rachithooda18@gmail.com>
2022-06-18 22:12:35 +0530

    designed frontend

---
+++ client/src/components/Transictions.jsx
@@ -0,0 +1,5 @@
+const Transictions = () => {
+  return <h1>Transictions</h1>;
+};
+
+export default Transictions;
---
+++ client/tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tesx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
